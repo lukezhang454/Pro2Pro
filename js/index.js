0b09bf45a169dd706776c7aafd7e9ccbd0ae60d8
lukezhang454 <lukezhang454@hotmail.com>
2017-11-04 13:24:40 -0700

    integrate frontend with server api and add season/region dropdowns

--- js/index.js
+++ js/index.js
@@ -4,52 +4,114 @@ import 'angular-ui-bootstrap';
 
 var index = angular.module('index', ['ui.bootstrap']);
 
-var url = "http://api.lolesports.com/api/v2/tournamentPlayerStats?groupName=regular_season&tournamentId=9c67f1fe-34d8-445b-a93f-d93e0ecd0056";
-var teams = ["TSM", "C9", "CLG", "DIG", "IMT", "FOX", "FLY", "P1", "NV", "TL"];
-var tableDict = { Name: "", Kda: "", CSPerMin: "" };
+var baseUrl = "https://ryany.org/pro2pro/api";
+var SEASONS = baseUrl + "/seasons";
+var tableDict = { Name: "", GamesPlayed: "", Kills: "", Deaths: "", Assists: "" };
 
-index.controller('ctrl1', function($scope) {
-	//Shouldn't have to make a request each time a team is selected
-	function getPlayersByTeam(response, teamId, side){
-		let players = [];
-		response.stats.forEach(function(player){
-			if(player.team === teamId) players.push({name:player.name, kda: player.kda, csPerMin: player.csPerMin});
-		});
-		if(side==='left'){
-			$scope.players1 = players;
-		}
-		else if(side==='right'){
-			$scope.players2 = players;
-		}
-	}
-	
-	$scope.onTeamChange = function(selectedTeam, side){
-		$.get(url, function( response ) {
-			if(side === 'left'){
-				$scope.$apply(getPlayersByTeam(response, $scope.selectedTeam1,side));
-			}
-			else if(side ==='right'){
-				$scope.$apply(getPlayersByTeam(response, $scope.selectedTeam2,side));
-			}
-		});
-	}
-	
-	$scope.setPlayer = function(selectedPlayer, side){
-		if(side === 'left'){
-			$scope.stats1 = {};
-			$scope.stats1.Name = selectedPlayer.name;
-			$scope.stats1.Kda = selectedPlayer.kda;
-			$scope.stats1.CSPerMin = selectedPlayer.csPerMin;
-		}
-		else if(side === 'right'){
-			$scope.stats2 = {};
-			$scope.stats2.Name = selectedPlayer.name;
-			$scope.stats2.Kda = selectedPlayer.kda;
-			$scope.stats2.CSPerMin = selectedPlayer.csPerMin;
-		}
-	};
-	
-	$scope.teams = teams;
-	$scope.stats1 = tableDict;
-	$scope.stats2 = tableDict;
+index.controller('homeController', function($scope) {
+    //Shouldn't have to make a request each time a team is selected
+    function getPlayersByTeam(response, side){
+        let players = [];
+        response.forEach(function(player){
+            players.push(
+                {
+                    name:player.name, 
+                    gamesPlayed: player.gamesPlayed,
+                    kills: player.kills, 
+                    deaths: player.deaths,
+                    assists: player.assists
+                });
+        });
+        if(side==='left'){
+            $scope.players1 = players;
+        }
+        else if(side==='right'){
+            $scope.players2 = players;
+        }
+    }
+
+    function getTeamsByRegion(response, side){
+        if(side==='left'){
+            $scope.teams1 = response;
+        }
+        else if(side==='right'){
+            $scope.teams2 = response;
+        }
+    }
+    
+    function getSeasons(){
+        return new Promise ((resolve, reject) =>{
+            let seasons = [];
+            $.get(SEASONS, function(response){
+                response.forEach(function(season){
+                    seasons.push(season);
+                });
+                $scope.seasons = seasons;
+                $scope.$apply();
+                resolve($scope.seasons[0]);
+            });
+        });
+    }
+    
+    function getRegions(season){
+        let regions = [];
+        $.get(baseUrl + `/seasons/${season}/regions`, function(response){
+            response.forEach(function(region){
+                regions.push(region);
+            });
+            $scope.regions = regions;
+            $scope.$apply();
+        });
+    }
+    
+    $scope.onTeamChange = function(selectedTeam, side){
+
+        if(side === 'left'){
+            $.get(baseUrl + `/seasons/${$scope.$ctrl.selectedSeason}/regions/${$scope.selectedRegion1}/teams/${selectedTeam}`,
+                function( response ) {
+                    $scope.$apply(getPlayersByTeam(response, side));
+            });
+        }
+        else if(side ==='right'){
+            $.get(baseUrl + `/seasons/${$scope.$ctrl.selectedSeason}/regions/${$scope.selectedRegion2}/teams/${selectedTeam}`,
+                function( response ) {
+                    $scope.$apply(getPlayersByTeam(response, side));
+            });
+        };
+    }
+    
+    $scope.onRegionChange = function(selectedRegion, side){
+        $.get(baseUrl + `/seasons/${$scope.$ctrl.selectedSeason}/regions/${selectedRegion}/teams`,
+            function( response ) {
+                if(side === 'left'){
+                    $scope.$apply(getTeamsByRegion(response, side));
+                }
+                else if(side ==='right'){
+                    $scope.$apply(getTeamsByRegion(response,side));
+                }
+        });
+    }
+    
+    $scope.setPlayer = function(selectedPlayer, side){
+        if(side === 'left'){
+            $scope.stats1 = {};
+            $scope.stats1.Name = selectedPlayer.name;
+            $scope.stats1.GamesPlayed = selectedPlayer.gamesPlayed;
+            $scope.stats1.Kills = selectedPlayer.kills;
+            $scope.stats1.Deaths = selectedPlayer.deaths;
+            $scope.stats1.Assists = selectedPlayer.assists;
+        }
+        else if(side === 'right'){
+            $scope.stats2 = {};
+            $scope.stats2.Name = selectedPlayer.name;
+            $scope.stats2.GamesPlayed = selectedPlayer.gamesPlayed;
+            $scope.stats2.Kills = selectedPlayer.kills;
+            $scope.stats2.Deaths = selectedPlayer.deaths;
+            $scope.stats2.Assists = selectedPlayer.assists;
+        }
+    };
+    
+    getSeasons().then(getRegions);
+    $scope.stats1 = tableDict;
+    $scope.stats2 = tableDict;
 })
